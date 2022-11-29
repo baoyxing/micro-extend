68d88f524154fc0ad32695a4fbf32b9fb20247e1
baoyx <byx19870908@yeah.net>
2022-11-29 15:07:22 +0800

    新增zap encoder config

--- pkg/utils/logutils/logger.go
+++ pkg/utils/logutils/logger.go
@@ -1,6 +1,9 @@
 package logutils
 
-import "github.com/natefinch/lumberjack"
+import (
+	"github.com/natefinch/lumberjack"
+	"go.uber.org/zap/zapcore"
+)
 
 func NewLogger(opts ...Option) *lumberjack.Logger {
 	config := DefaulutLumberjackConfig()
@@ -14,5 +17,22 @@ func NewLogger(opts ...Option) *lumberjack.Logger {
 		MaxBackups: config.maxBackups,
 		LocalTime:  config.localTime,
 		Compress:   config.compress,
+	}
+}
+
+func NewZapEncoderConfig() *zapcore.EncoderConfig {
+	return &zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "linenum",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,  // 小写编码器
+		EncodeTime:     zapcore.ISO8601TimeEncoder,     // ISO8601 UTC 时间格式
+		EncodeDuration: zapcore.SecondsDurationEncoder, //
+		EncodeCaller:   zapcore.FullCallerEncoder,      // 全路径编码器
+		EncodeName:     zapcore.FullNameEncoder,
 	}
 }
